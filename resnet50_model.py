6584a5ce4551fa3b8a4130e1c8feb8b3afc8897d
KHSMT <khsmtlab122@gmail.com>
2022-12-15 19:02:21 +0900

    resnet50_model.py fixed

--- resnet50_model.py
+++ resnet50_model.py
@@ -1,3 +1,22 @@
+#two heads resnet50 backbone problem:target layers
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import cv2
 from torch.utils.data import Dataset
 from pathlib import Path
 from typing import List, Tuple
@@ -17,13 +36,14 @@ import torch.optim as optim
 from torchvision.models import feature_extraction
 from torchvision.models.feature_extraction import create_feature_extractor
 from torchvision.models.feature_extraction import get_graph_node_names
+from torchvision.models._utils import IntermediateLayerGetter
+
 
 import torch
 import torchvision
 from PIL import Image
 from torchvision import transforms
 import os
-
 from torchvision.transforms import InterpolationMode
 BICUBIC = InterpolationMode.BICUBIC
 
@@ -39,6 +59,8 @@ from pytorch_grad_cam import GradCAM,EigenCAM
 
 model = models.resnet50(pretrained = True)
 names = torchvision.models.feature_extraction.get_graph_node_names(model)
+
+print(model)
 '''
 print(names)
 
@@ -59,19 +81,46 @@ def print_model(module, name="model", depth=0):
 
 print_model(model)
 '''
+
+def imshow(img,num):
+    # 非正規化する
+    #img = img / 2 + 0.5
+    # torch.Tensor型からnumpy.ndarray型に変換する
+    #print(type(img)) # <class 'torch.Tensor'>
+    #print(img.shape)
+    npimg = img.to('cpu').detach().numpy().copy()
+    
+    #print(type(npimg))    
+    # 形状を（RGB、縦、横）から（縦、横、RGB）に変換する
+    #print(npimg.shape)
+    npimg = np.transpose(npimg, (1, 2, 0))
+    #print(npimg.shape)
+    #npimg.save('./result/cifar10_vis'+dt_now_str+'.jpg',npimg*255)
+    
+    #print(npimg)
+    npimg=cv2.cvtColor(npimg,cv2.COLOR_BGR2RGB)
+    cv2.imwrite('./result/cifar10_'+dt_now_str+str(eps)+'_EPS.jpg',npimg*255)
+    
+ 
+
 class Network(nn.Module):
     def __init__(self):
         super().__init__()
         self.backbone = models.resnet50(pretrained=True)
-        self.backbone = create_feature_extractor(self.backbone, ["flatten"])
+
+        self.backbone = IntermediateLayerGetter(self.backbone,return_layers={"avgpool":"out"})
+        self.flatten =  nn.Flatten()
+        #self.backbone = create_feature_extractor(self.backbone, ["flatten"])
         self.fc1 = nn.Sequential(
-            nn.Linear(2048, 10)
+                nn.Linear(2048, 10)
         )
         self.fc2 = nn.Sequential(
             nn.Linear(2048, 10)
         )
-    def forward(self, x, change_head: str):
-        x = self.backbone(x)["flatten"]
+    def forward(self, x, change_head='normal'):
+        #x = self.backbone(x)["out"]
+        x = self.backbone(x)["out"]
+        x = self.flatten(x)
         if change_head == "normal":
             x = self.fc1(x)
         elif change_head == "g_normal":
@@ -147,27 +196,34 @@ test_load = torch.utils.data.DataLoader(
 names = ("plane", "car", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck")
 
 
-fx_model = models.resnet50(pretrained = True)
-fx_model.fc = nn.Linear(2048,10)
+#fx_model = models.resnet50(pretrained = True)
+#fx_model.fc = nn.Linear(2048,10)
 #print(fx_model)
-#fx_model = Network()
-fx_model = fx_model.to("cuda")
 
-#print(fx_model.backbone.layer2)
-#print(getattr(fx_model.backbone.layer2, '0'))
+fx_model = Network()
+
+#x_model = fx_transform(fx_model)
+fx_model = fx_model.to("cuda")
+print(fx_model)
+
+'''
 optimizer = optim.SGD(
         fx_model.parameters(),
-        lr=0.01,
+        lr=0.05,
         momentum=0.9,
-        weight_decay=0.00005
         )
-scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
+
+'''
+optimizer = optim.Adam(fx_model.parameters(),lr=0.005)
+scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=100)
+#scheduler = torch.optim.lr_scheduler.LinearLR(optimizer, start_factor=1, end_factor=0.1, total_iters=50)
+
 criterion = nn.CrossEntropyLoss()
 
 
-atk = PGD(fx_model,eps=4/255,alpha = 8/255,steps=2, random_start=False)
-atk = FGSM(fx_model,eps=2/255)
-atk.set_normalization_used(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+#atk = PGD(fx_model,eps=4/255,alpha = 8/255,steps=4, random_start=False)
+#atk = FGSM(fx_model,eps=2/255)
+#atk.set_normalization_used(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
 
 
@@ -183,7 +239,7 @@ model_file="ckpt2"
 eps=64/255
 alpha=2/255
 step=4
-cam_mode = "eigen"
+cam_mode = "grad_cam"
 #####################################################################################
 for epoch in range(num_epochs):
     fx_model.train()
@@ -204,63 +260,61 @@ for epoch in range(num_epochs):
         labels = labels.to(device)
         
         optimizer.zero_grad()
-        outputs = fx_model(imgs)
+        outputs = fx_model(imgs,"normal")
+        
         loss = criterion(outputs, labels)
+        
         loss.backward()
+        running_loss += loss.item()
+        pred = torch.argmax(outputs, dim=1)
+        running_acc += torch.mean(pred.eq(labels).float())
+        
         optimizer.step()
+        #cam_img forward
 
         fx_model.eval()
-
-        
-
-
 
         c_labels = c_labels.to(device)
         norm = transforms.Compose([
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ])
         input_img = norm(c_imgs)     #<torch.float32>
-        rgb_img = c_imgs 
-            
+        rgb_img = c_imgs     
         fx_model.eval()
+        target_layers = [
+            getattr(fx_model.backbone.layer1, '2'), 
+            getattr(fx_model.backbone.layer2, '3'), 
+            getattr(fx_model.backbone.layer4, '2'),
+            getattr(fx_model.backbone.layer3, '5')]
+        '''
+        target_layers = [
+            fx_model.backbone.layer1[-1],
+            fx_model.backbone.layer2[-1],
+            fx_model.backbone.layer3[-1],
+            fx_model.backbone.layer4[-1]
+            ]  '''
         
-        #target_layers = [getattr(fx_model.backbone.layer1, '2'), getattr(fx_model.backbone.layer2, '3'), getattr(fx_model.backbone.layer4, '2'),getattr(fx_model.backbone.layer3, '5')]
-        
-        print("*"*80)
-        #print(target_layers)
-        #target_layers = [fx_model.backbone.module.layer2[-1],fx_model.backbone.module.layer2[-1],fx_model.backbone.module.layer3[-1]]
-        target_layers = [fx_model.layer3[-1],fx_model.layer4[-1],fx_model.layer2[-1],fx_model.layer1[-1]]
-        
-        #input_img = input_img[0].to("cuda")
         input_img = input_img.to("cuda")
         #input_img = input_img.unsqueeze(0) 
        
-        print(input_img.shape)
-        '''
 
-
-        cam = GradCAM(
-            model=fx_model, target_layers=target_layers, use_cuda=torch.cuda.is_available()
-        )
-        cam.batch_size = 1
-        #input_img = input_img.squeeze(0)
-        print(input_img.shape)
-        grayscale_cam = cam(
-            input_tensor=input_img,
-            #targets=[ClassifierOutputTarget(label)],
-            targets=None
-        )
-        
-        
-        
-        '''
-        with GradCAM(model=fx_model, target_layers=target_layers, use_cuda=torch.cuda.is_available()) as cam:
+        if cam_mode == "grad_cam":
+            with GradCAM(model=fx_model, target_layers=target_layers, use_cuda=torch.cuda.is_available()) as cam:
                 cam.batch_size=train_batch
                 grayscale_cam = cam(
                 #input_tensor=input_img.unsqueeze(0),
                 input_tensor = input_img,
                 #targets=[ClassifierOutputTarget(l.item()) for l in self.label],
                 targets=None)#(128, 32, 32)                        
+        elif cam_mode == "eigen":    
+            with GradCAM(model=fx_model, target_layers=target_layers, use_cuda=torch.cuda.is_available()) as cam:
+                cam.batch_size=train_batch
+                grayscale_cam = cam(
+                #input_tensor=input_img.unsqueeze(0),
+                input_tensor = input_img,
+                #targets=[ClassifierOutputTarget(l.item()) for l in self.label],
+                targets=None)#(128, 32, 32)   
+
         fx_model.train()
         rgb_img = torch.squeeze(rgb_img)
     
@@ -274,16 +328,128 @@ for epoch in range(num_epochs):
                     
         rgb_img = torch.permute(rgb_img,(1,0,2,3))
         rgb_imgs = rgb_img.to(device)
-        #cam_imgs = cam_imgs.to('cpu').detach().numpy().copy()
-        #ims(torchvision.utils.make_grid(cam_imgs),128)     
+        
         optimizer.zero_grad()
-        c_outputs = fx_model(rgb_imgs)
-        loss = criterion(c_outputs, c_labels)
-            
-        loss.backward()
+        c_outputs = fx_model(rgb_imgs,"g_normal")
+        #c_outputs = fx_model(rgb_imgs)
+        c_loss = criterion(c_outputs, c_labels)
+    
+        model_loss = loss + c_loss    
+        c_loss.backward()
+        
+        #model_loss.backward()
+        running_loss += c_loss.item()
+        pred = torch.argmax(c_outputs, dim=1)
+        running_acc += torch.mean(pred.eq(c_labels).float())
         optimizer.step()
+    #imshow(torchvision.utils.make_grid(rgb_img),eps)
+    running_loss /= len(trainloader)
+    running_acc /= len(trainloader)
 
+
+    #validation
+    fx_model.eval()
+    val_running_loss = 0.0
+    val_g_running_acc = 0.0
+    val_n_running_acc = 0.0
+    for val_imgs, val_labels in tqdm(val_load):
+        val_c_imgs = val_imgs
+        val_c_labels = val_labels
+
+       
+        
+        norm = transforms.Compose([
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+        val_imgs = norm(val_imgs)     #<torch.float32>
+        val_imgs = val_imgs.to(device)
+        val_labels = val_labels.to(device)
+        val_outputs = fx_model(val_imgs,"g_normal")
+        val_loss = criterion(val_outputs, val_labels)
+        
+        val_running_loss += val_loss.item()
+        val_pred = torch.argmax(val_outputs, dim=1)
+        val_n_running_acc += torch.mean(val_pred.eq(val_labels).float())
+
+        
+        #cam_img forward
+        val_c_labels = val_c_labels.to(device)
+        norm = transforms.Compose([
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+        val_input_img = norm(val_c_imgs)     #<torch.float32>
+        val_rgb_img = val_c_imgs 
+            
+        
+                
+        target_layers = [
+            getattr(fx_model.backbone.layer1, '2'), 
+            getattr(fx_model.backbone.layer2, '3'), 
+            getattr(fx_model.backbone.layer4, '2'),
+            getattr(fx_model.backbone.layer3, '5')]
+        
+        
+        '''
+        target_layers = [
+            fx_model.backbone.layer1[-1],
+            fx_model.backbone.layer2[-1],
+            fx_model.backbone.layer3[-1],
+            fx_model.backbone.layer4[-1]
+            ]  '''
+                      
+        #input_img = input_img[0].to("cuda")
+        val_input_img = val_input_img.to("cuda")
+        #input_img = input_img.unsqueeze(0) 
+       
+
+        if cam_mode == "grad_cam":
+            with GradCAM(model=fx_model, target_layers=target_layers, use_cuda=torch.cuda.is_available()) as cam:
+                cam.batch_size=val_batch
+                val_grayscale_cam = cam(
+                #input_tensor=input_img.unsqueeze(0),
+                input_tensor = val_input_img,
+                #targets=[ClassifierOutputTarget(l.item()) for l in self.label],
+                targets=None)#(128, 32, 32)                        
+        elif cam_mode == "eigen":    
+            with GradCAM(model=fx_model, target_layers=target_layers, use_cuda=torch.cuda.is_available()) as cam:
+                cam.batch_size=val_batch
+                val_grayscale_cam = cam(
+                #input_tensor=input_img.unsqueeze(0),
+                input_tensor = val_input_img,
+                #targets=[ClassifierOutputTarget(l.item()) for l in self.label],
+                targets=None)#(128, 32, 32)   
+
+        
+        val_rgb_img = torch.squeeze(val_rgb_img)
+    
+        val_grayscale_cam = np.array(val_grayscale_cam,dtype=np.float32)
+        val_grayscale_cam = torch.from_numpy(val_grayscale_cam)
+        val_rgb_img = torch.permute(val_rgb_img,(1,0,2,3))
+        val_rgb_img = val_rgb_img.to("cuda")
+        val_grayscale_cam = val_grayscale_cam.to("cuda")
+        
+        val_rgb_img = val_grayscale_cam*val_rgb_img  
+                    
+        val_rgb_img = torch.permute(val_rgb_img,(1,0,2,3))
+        val_rgb_imgs = val_rgb_img.to(device)
+   
+        
+        val_c_outputs = fx_model(val_rgb_imgs,"g_normal")
+        val_c_loss = criterion(val_c_outputs, val_c_labels)
+            
+        val_running_loss += val_c_loss.item()
+        val_pred = torch.argmax(val_c_outputs, dim=1)
+        val_g_running_acc += torch.mean(val_pred.eq(val_c_labels).float())
+    imshow(torchvision.utils.make_grid(val_rgb_imgs),eps)
+    
+    val_running_loss /= len(val_load)
+    val_n_running_acc /= len(val_load)
+    val_g_running_acc /= len(val_load)
+
+    print(val_n_running_acc,val_g_running_acc)
+    print("epoch: {}, loss: {}, acc: {}, \
+            val loss: {}, val acc: {}".format(epoch, running_loss/2, running_acc/2, val_running_loss/2, (val_g_running_acc+val_n_running_acc)/2))
+                
         
 
 
-    
